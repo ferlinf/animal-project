14feeb9f48c475410107416b804ff2d705afff20
ferlinf <ferlinelin7@gmail.com>
2023-11-10 08:07:35 +0700

    ui

---
+++ app/src/main/java/com/example/projectanimals/KepitingActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectanimals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KepitingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kepiting);
+    }
+}
